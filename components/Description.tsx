4edbfacd7780bf1ac8724e6ac737629da48a91e1
Mateusz Lisowski <lisek091@gmail.com>
2023-04-17 17:58:23 +0200

    lorem ipsum delelte

--- components/Description.tsx
+++ components/Description.tsx
@@ -4,18 +4,17 @@ const Description = () =>{
             <div className="hidden md:flex sm:w-1/5"></div>
             <div className="sm:w-1/5 w-2/5">
                 <h1>Dlaczego my?</h1>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</h3>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</h3>
+                <h3>Tutaj będzie opis</h3>
+                <h3>Tutaj będzie opis</h3>
             </div>
             <div className="border-2 border-blue-300 mx-8 sm:border-spacing-6"></div>
             <div className="md:w-2/5 w-3/5">
                 <h2>Dodatkowy opis firmy</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru</p>
+                    <h3>Tutaj będzie opis</h3>
+                    <h3>Tutaj będzie opis</h3>
+                    <h3>Tutaj będzie opis</h3>
+                    <h3>Tutaj będzie opis</h3>
+                    <h3>Tutaj będzie opis</h3>
             </div>
             <div className="hidden xl:flex xl:w-1/5"></div>
         </div>
