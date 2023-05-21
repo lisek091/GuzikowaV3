71198275c4f6ad2fd85bf91c76fcb2b939858140
Mateusz Lisowski <lisek091@gmail.com>
2023-05-21 20:11:32 +0200

    poprawione fotki

--- components/Work.tsx
+++ components/Work.tsx
@@ -16,10 +16,10 @@ const Work = () =>{
             <div className="hidden md:block w-36 lg:w-96"></div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 place-items-center">
-            <ServiceCard link={"./oferta/okna"} Przycisk="Okna" Background="cardBackground2" Tytul="Okna" Opis="Są bardzo wytrzymałe i odporne na uszkodzenia mechaniczne oraz warunki atmosferyczne, takie jak wiatr, deszcz czy słońce."/>
-            <ServiceCard link={"./oferta/uslugibudowlane"} Przycisk="Usługi budowlane" Background="serviceCard2" Tytul="Usługi budowlane" Opis="Zamówienie naszych usług montażowych jest bardzo proste. Wystarczy skontaktować się z nami za pomocą formularza kontaktowego na naszej stronie internetowej lub telefonicznie."/>
-            <ServiceCard link={"./oferta/systemy"} Przycisk="Systemy przesuwne" Background="serviceCard1" Tytul="Systemy przesuwne" Opis="Najwyższej jakościu produkty. Więcej informacji wkrótce."/>
-            <ServiceCard link={"./oferta/uslugi"} Przycisk="Ochrona przeciwsłoneczna" Background="serviceCard3" Tytul="Ochrona przeciwsłoneczna" Opis="Najwyższej jakościu produkty. Więcej informacji wkrótce."/>
+            <ServiceCard link={"./oferta/okna"} Przycisk="Okna" Background="serviceCard1" Tytul="Okna" Opis="Są bardzo wytrzymałe i odporne na uszkodzenia mechaniczne oraz warunki atmosferyczne, takie jak wiatr, deszcz czy słońce."/>
+            <ServiceCard link={"./oferta/uslugibudowlane"} Przycisk="Usługi budowlane" Background="cardBackground2" Tytul="Usługi budowlane" Opis="Zamówienie naszych usług montażowych jest bardzo proste. Wystarczy skontaktować się z nami za pomocą formularza kontaktowego na naszej stronie internetowej lub telefonicznie."/>
+            <ServiceCard link={"./oferta/systemy"} Przycisk="Systemy przesuwne" Background="serviceCard3" Tytul="Systemy przesuwne" Opis="Najwyższej jakościu produkty. Więcej informacji wkrótce."/>
+            <ServiceCard link={"./oferta/uslugi"} Przycisk="Ochrona przeciwsłoneczna" Background="serviceCard2" Tytul="Ochrona przeciwsłoneczna" Opis="Najwyższej jakościu produkty. Więcej informacji wkrótce."/>
         </div>
     </div>
     )
